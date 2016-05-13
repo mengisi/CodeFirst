ea21266914cfb384051a0b396fb3b8b90e15fd4e
MengXue <xuemon@gmail.com>
2016-05-12 20:22:00 -0400

    raw text for rich text

---
+++ CodeFirst/Controllers/AnnoucementsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CodeFirst.Models;
+
+namespace CodeFirst.Controllers
+{
+    public class AnnoucementsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Annoucements
+        public ActionResult Index()
+        {
+            return View(db.Annoucements.ToList());
+        }
+
+        // GET: Annoucements/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Annoucement annoucement = db.Annoucements.Find(id);
+            if (annoucement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(annoucement);
+        }
+
+        // GET: Annoucements/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Annoucements/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Text,CreateTime,Updatetime,Visits")] Annoucement annoucement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Annoucements.Add(annoucement);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(annoucement);
+        }
+
+        // GET: Annoucements/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Annoucement annoucement = db.Annoucements.Find(id);
+            if (annoucement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(annoucement);
+        }
+
+        // POST: Annoucements/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Text,CreateTime,Updatetime,Visits")] Annoucement annoucement)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(annoucement).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(annoucement);
+        }
+
+        // GET: Annoucements/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Annoucement annoucement = db.Annoucements.Find(id);
+            if (annoucement == null)
+            {
+                return HttpNotFound();
+            }
+            return View(annoucement);
+        }
+
+        // POST: Annoucements/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Annoucement annoucement = db.Annoucements.Find(id);
+            db.Annoucements.Remove(annoucement);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- CodeFirst/Models/Annoucement.cs
+++ CodeFirst/Models/Annoucement.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace CodeFirst.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        [DataType(DataType.MultilineText)]
         public string Text { get; set; }
         public DateTime CreateTime { get; set; }
         public DateTime Updatetime { get; set; }
