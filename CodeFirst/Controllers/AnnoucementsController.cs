89c79af169055095bb0e51938b16c6caa79223e6
MengXue <xuemon@gmail.com>
2016-05-13 12:59:08 -0400

    correction erro : admin instead of Admin

--- CodeFirst/Controllers/AnnoucementsController.cs
+++ CodeFirst/Controllers/AnnoucementsController.cs
@@ -62,7 +62,7 @@ namespace CodeFirst.Controllers
         }
 
         // GET: Annoucements/Edit/5
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "admin")]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -81,7 +81,7 @@ namespace CodeFirst.Controllers
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost, ValidateInput(false)]
-        [Authorize(Roles = "Admin")]
+        [Authorize(Roles = "admin")]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,Title,Text,CreateTime")] Annoucement annoucement)
         {
